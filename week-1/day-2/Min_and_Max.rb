4a36f3139780eab5b603e57cd932a1fc0cad03ba
FrankFlores <fraco.1918@gmail.com>
2020-04-14 12:10:39 -0500

    Min_and_Max.rb

---
+++ week-1/day-2/Min_and_Max.rb
@@ -0,0 +1,26 @@
+numbers = [2, 56, 5, 3, 4, 6, 1, -3456, 123891, 454, 1, 23, 5, 7]
+
+def maxnum(array)
+     highest = 0
+
+     for a in 0..(array.length) do
+          if highest <= array[a].to_i
+               highest = array[a].to_i
+          end
+     end
+     puts "Highest: #{highest}"
+end
+
+def minnum(array)
+     lowest = 0
+
+     for a in 0..(array.length) do
+          if lowest >= array[a].to_i
+               lowest = array[a].to_i
+          end
+     end
+     puts "Lowest: #{lowest}"
+end
+
+maxnum(numbers)
+minnum(numbers)
